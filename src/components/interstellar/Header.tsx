f781616551d3effe8482fbe23bb77baf3505182e
Elias Ablan <eliasablan93@gmail.com>
2024-02-01 23:30:10 -0400

    change header fonts

--- src/components/interstellar/Header.tsx
+++ src/components/interstellar/Header.tsx
@@ -1,4 +1,9 @@
 import { cn } from '@/lib/utils'
+import { Roboto_Condensed } from 'next/font/google'
+
+const RobotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+})
 
 type props = {
   className?: string
@@ -6,13 +11,22 @@ type props = {
 
 export default function Header({ className }: props) {
   return (
-    <header className={cn('flex items-center justify-between', className)}>
-      <div className="flex flex-col leading-5 tracking-tight">
-        <h1>A Film by</h1>
-        <p className="font-bold">Christopher Nolan</p>
+    <header
+      className={cn(
+        'flex items-center justify-between',
+        RobotoCondensed.className,
+        className
+      )}
+    >
+      <div className="flex flex-col leading-5">
+        <h1 className="font-light">
+          A Film by
+          <br />
+          <span className="font-medium">Christopher Nolan</span>
+        </h1>
       </div>
 
-      <p className="border-2 border-[#1b1b1d] p-2 font-bold leading-none">
+      <p className="border-2 border-[#1b1b1d] p-2 text-lg font-bold leading-none">
         14
       </p>
     </header>
