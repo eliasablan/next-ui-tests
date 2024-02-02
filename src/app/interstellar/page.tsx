62df9c2c573c2b3f8093d98cd28904cc288e3cac
Elias Ablan <eliasablan93@gmail.com>
2024-02-01 20:13:42 -0400

    Interstellar page and Link in homepage

---
+++ src/app/interstellar/page.tsx
@@ -0,0 +1,7 @@
+export default function Interstellar() {
+  return (
+    <main className="relative min-h-svh w-svw bg-[#e7e6db]">
+      <h1 className="text-4xl font-bold">Interstellar</h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
+
+const pages = [
+  {
+    title: 'Interstellar',
+    href: 'interstellar',
+  },
+]
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Welcome to my Next.js app</h1>
+    <main className="mx-auto max-w-2xl">
+      <h1 className="my-4 text-4xl font-bold">UI tests</h1>
+      {pages.map((page) => (
+        <div className="block py-2">
+          <Link
+            className="font-semibold text-blue-800 hover:underline"
+            href={page.href}
+          >
+            {page.title}
+          </Link>
+        </div>
+      ))}
     </main>
-  );
+  )
 }
