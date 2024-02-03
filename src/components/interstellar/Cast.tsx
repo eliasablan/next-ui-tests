343545f7c9613af09ab2e6fdfa99b2916e58e3f6
Elias Ablan <eliasablan93@gmail.com>
2024-02-03 14:54:46 -0400

    Update Cast and Title sizing in smallest screens

--- src/components/interstellar/Cast.tsx
+++ src/components/interstellar/Cast.tsx
@@ -12,21 +12,21 @@ export default function Cast({ className }: props) {
       initial={{ x: 100, opacity: 0 }}
       animate={{ x: 0, opacity: 1, transition: { delay: 0.2 } }}
       className={cn(
-        'mt-14 flex justify-end gap-x-8 text-xs font-semibold tracking-tight',
+        'mt-14 flex justify-end gap-x-4 text-xs font-semibold tracking-tight sm:gap-x-8',
         className
       )}
     >
-      <div className="flex flex-col justify-between gap-y-4 sm:gap-y-1">
+      <div className="flex flex-col justify-between gap-y-2 sm:gap-y-1">
         <p>Matthew McConaughey</p>
         <p>Anne Hathaway</p>
         <p>Jessica Chastain</p>
       </div>
-      <div className="flex flex-col justify-between gap-y-4 sm:gap-y-1">
+      <div className="flex flex-col justify-between gap-y-2 sm:gap-y-1">
         <p>Mackenzie Foy</p>
         <p>Bill Irwin</p>
         <p>Michael Caine</p>
       </div>
-      <div className="flex flex-col justify-between gap-y-4 sm:gap-y-1">
+      <div className="flex flex-col justify-between gap-y-2 sm:gap-y-1">
         <p>John Lithgow</p>
         <p>David Oyellowo</p>
         <p>Ellen Burstyn</p>
