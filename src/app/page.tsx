1ad7b6e8d23400ee02f00bb580db278e17eece33
Elias Ablan <eliasablan93@gmail.com>
2024-02-29 10:09:07 -0400

    Refactor home navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,37 @@ const pages = [
     title: 'Interstellar',
     href: 'interstellar',
   },
+  {
+    title: 'Background Beams',
+    href: 'background-beams',
+  },
+  {
+    title: 'Meteors',
+    href: 'meteors',
+  },
+  {
+    title: 'Particles',
+    href: 'Particles',
+  },
 ]
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-2xl p-4">
       <h1 className="text-4xl font-bold">UI tests</h1>
-      {pages.map((page) => (
-        <div key={page.href} className="block py-2">
-          <Link
-            className="font-semibold text-blue-800 hover:underline"
-            href={page.href}
-          >
-            {page.title}
-          </Link>
-        </div>
-      ))}
+
+      <ul className="mt-4 list-inside list-disc">
+        {pages.map((page) => (
+          <li key={page.href}>
+            <Link
+              className="font-semibold text-blue-800 hover:underline"
+              href={page.href}
+            >
+              {page.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
