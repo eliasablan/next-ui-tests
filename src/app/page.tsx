7d1689505df0c5573663f6021e5bce1c25c37642
Elias Ablan <eliasablan93@gmail.com>
2024-02-29 16:52:36 -0400

    homepage background-boxes effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Boxes } from '@/components/ui/background-boxes'
 import Link from 'next/link'
 
 const pages = [
@@ -21,14 +22,17 @@ const pages = [
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-2xl p-4">
-      <h1 className="text-4xl font-bold">UI tests</h1>
+    <main className="relative mx-auto min-h-svh w-full max-w-2xl overflow-hidden bg-slate-900 p-4">
+      <Boxes />
+      <h1 className="relative z-20 text-4xl font-bold text-white">
+        UI tests
+      </h1>
 
-      <ul className="mt-4 list-inside list-disc">
+      <ul className="mt-4 list-inside list-disc text-emerald-600">
         {pages.map((page) => (
           <li key={page.href}>
             <Link
-              className="font-semibold text-blue-800 hover:underline"
+              className="relative z-20 font-semibold text-red-500 hover:underline"
               href={page.href}
             >
               {page.title}
